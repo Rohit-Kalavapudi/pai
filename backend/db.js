417367fd2ecaa351b0064f8efa5f8e4116427a9d
Rohit <rohit.kalavapudi@gmail.com>
2023-10-14 17:49:18 +0530

    comm

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const mongoURI = 'mongodb://0.0.0.0:27017/'        
+const mongoURI = 'mongodb+srv://rohit:admin@cluster0.qz5l5.mongodb.net/'        
 
 const connectToMongo = async()=>{
     await mongoose.connect(mongoURI)
--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const port = 4000
 app.use(express.json())
 // app.use(cors)
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3000','*'
   }));
 
  app.use('/api/auth', require('./routes/auth'))
