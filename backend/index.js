0998ec8662f60ebd2fe71ba6c24d5a8c17a90948
Rohit <rohit.kalavapudi@gmail.com>
2023-10-14 17:51:55 +0530

    comm

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const openai = new OpenAIApi(configuration);
 
 connectToMongo()
 const app = express()
-const port = 4000
+const port = process.env.PORT||4000
 
 app.use(express.json())
 // app.use(cors)
