c0d4c7e787731350a717c1850f3e1b03f4f313b6
Rohit-Kalavapudi <93667768+Rohit-Kalavapudi@users.noreply.github.com>
2023-10-25 20:10:40 +0530

    Update login.js

--- powerai/src/components/login.js
+++ powerai/src/components/login.js
@@ -8,7 +8,7 @@ const Login = (props) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:4000/api/auth/login", {
+        const response = await fetch("https://pai-backend.onrender.com", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
