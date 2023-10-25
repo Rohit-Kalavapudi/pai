bd8864636e6faa38b60b1a52c2c779479278df8e
Rohit-Kalavapudi <93667768+Rohit-Kalavapudi@users.noreply.github.com>
2023-10-25 20:41:14 +0530

    Update Signup.js

--- powerai/src/components/Signup.js
+++ powerai/src/components/Signup.js
@@ -8,7 +8,7 @@ const Signup = (props) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:4000/api/auth/createuser", {
+        const response = await fetch("https://pai-backend.onrender.com", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
